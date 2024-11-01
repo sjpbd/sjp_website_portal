f8d6b3d2574a3a9226efdde481700e4a4a823083
Racy Daniel Godino <brracy@gmail.com>
2024-11-01 08:53:23 +0600

    Updated settings.py with recent changes

--- core/settings.py
+++ core/settings.py
@@ -158,7 +158,7 @@ TINYMCE_DEFAULT_CONFIG = {
 # Database configuration
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
