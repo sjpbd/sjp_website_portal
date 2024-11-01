464189c7ce5cf03f99c8114dfc1d2aa2bd4e4bf4
Racy Daniel Godino <brracy@gmail.com>
2024-11-01 09:06:08 +0600

    Updated settings.py with recent changes

--- core/settings.py
+++ core/settings.py
@@ -163,7 +163,7 @@ DATABASES = {
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'PORT': os.getenv('DB_PORT', '3306'), 
     }
 }
 
